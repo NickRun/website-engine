10b7816e530bd260c0d7a1119d990e1ac7a75860
Nick Scipione <nscipione@gmail.com>
2016-04-03 23:12:31 -0400

    style

--- src/Site.vue
+++ src/Site.vue
@@ -15,11 +15,12 @@ export default {
 @import "./scss/mixins/clearfix";
 @import "./scss/mixins/btn-sizes";
 @import "./scss/utilities/visibility";
+@import "./scss/utilities/responsive-table";
+@import "./scss/utilities/no-top-margin";
 @import "./scss/partials/global";
 @import "./scss/partials/typography";
 @import "./scss/partials/buttons";
 @import "./scss/partials/code";
-@import "./scss/partials/tables";
 @import "./scss/partials/forms";
 @import "./scss/partials/canvas";
 @import "./scss/partials/grid";
