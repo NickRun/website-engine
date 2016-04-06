dfcbde6e305732c8b2b094c5060a5c9276c8d662
Nick Scipione <nscipione@gmail.com>
2016-04-06 13:36:09 -0400

    google analytics tracking

--- src/main.js
+++ src/main.js
@@ -45,3 +45,14 @@ router.map({
 })
 
 router.start(Site, 'body')
+
+// Google Analytics
+/*ignore jslint start*/
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-76044572-1', 'auto');
+ga('send', 'pageview');
+/*ignore jslint end*/
